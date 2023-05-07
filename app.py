6b2c1649dbba81f7b4f8c740263c2b398d1355cc
AI Anytime <sonu1000raw@gmail.com>
2023-05-07 19:39:41 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from textblob import TextBlob
+
+app = FastAPI(title="Sentiment Analysis API")
+
+@app.get("/", include_in_schema=False)
+def index():
+    return RedirectResponse("/docs", status_code=308)
+
+@app.get("/sentiment-analysis/{text}")
+def sentiment_analysis(text: str):
+    blob = TextBlob(text)
+    polarity = blob.sentiment.polarity
+    subjectivity = blob.sentiment.subjectivity
+
+    if polarity > 0:
+        sentiment = "positive"
+    elif polarity < 0:
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+
+    return {
+        "text": text,
+        "sentiment": sentiment,
+        "polarity": polarity,
+        "subjectivity": subjectivity
+    }
